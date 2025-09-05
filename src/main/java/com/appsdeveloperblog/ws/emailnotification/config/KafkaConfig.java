9336d779489d1e5ead7747b48839de67c6021d68
JasonOCat <jason.catbagan@gmail.com>
2025-09-05 06:08:42 +0200

    86. Handle errors: The DefaultErrorHandler and DeadLetterPublishingRecoverer classes
    Introduce `DeadLetterPublishingRecoverer` and `DefaultErrorHandler` to handle message consumption errors. Updated the `kafkaListenerContainerFactory` to set a common error handler, improving reliability and fault tolerance in Kafka message processing.

--- src/main/java/com/appsdeveloperblog/ws/emailnotification/config/KafkaConfig.java
+++ src/main/java/com/appsdeveloperblog/ws/emailnotification/config/KafkaConfig.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
@@ -40,9 +43,12 @@ public class KafkaConfig {
     }
 
     @Bean
-    ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+    ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(KafkaTemplate<String, Object> kafkaTemplate) {
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler(new DeadLetterPublishingRecoverer(kafkaTemplate));
+
         ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
+        factory.setCommonErrorHandler(errorHandler);
         return factory;
     }
 
