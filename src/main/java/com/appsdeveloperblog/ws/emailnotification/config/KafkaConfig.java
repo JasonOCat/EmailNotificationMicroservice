6c0bb6e34a8e2f8e857bba814852a2962adf2dfa
JasonOCat <jason.catbagan@gmail.com>
2025-09-05 17:03:06 +0200

    87. Create and Configure KafkaTemplate object, Add Kafka producer configuration for Dead Letter Topic handling
    This commit introduces a producer configuration in `application.yml` and updates `KafkaConfig` to include support for KafkaTemplate and ProducerFactory. A new `KafkaProducerProperties` class is added to manage producer-related properties. These changes enable Dead Letter Topic handling with proper serialization setup.

--- src/main/java/com/appsdeveloperblog/ws/emailnotification/config/KafkaConfig.java
+++ src/main/java/com/appsdeveloperblog/ws/emailnotification/config/KafkaConfig.java
@@ -1,29 +1,30 @@
 package com.appsdeveloperblog.ws.emailnotification.config;
 
+import com.appsdeveloperblog.ws.products.ProductCreatedEvent;
 import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.*;
 import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@RequiredArgsConstructor
 public class KafkaConfig {
 
     private final KafkaConsumerProperties kafkaConsumerProperties;
 
-    public KafkaConfig(KafkaConsumerProperties kafkaConsumerProperties) {
-        this.kafkaConsumerProperties = kafkaConsumerProperties;
-    }
+    private final KafkaProducerProperties kafkaProducerProperties;
 
     @Bean
     ConsumerFactory<String, Object> consumerFactory() {
@@ -50,6 +51,21 @@ public class KafkaConfig {
         factory.setConsumerFactory(consumerFactory());
         factory.setCommonErrorHandler(errorHandler);
         return factory;
+    }
+
+    @Bean
+    KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProducerProperties.getBootstrapServers());
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, kafkaProducerProperties.getKeySerializer());
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
     }
 
 
