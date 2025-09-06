a483d99126c9b36732142444473202102c802736
JasonOCat <jason.catbagan@gmail.com>
2025-09-06 08:54:02 +0200

    Refactor Kafka configurations and simulate error handling.
    Updated Kafka consumer and producer serializers for JSON support and adjusted bootstrap server formatting. Added a simulated error in the `ProductCreatedEventHandler` to throw a `NotRetryableException`. Deprecated unused code related to BigDecimal decoding for cleanup.

--- src/main/java/com/appsdeveloperblog/ws/emailnotification/handler/ProductCreatedEventHandler.java
+++ src/main/java/com/appsdeveloperblog/ws/emailnotification/handler/ProductCreatedEventHandler.java
@@ -1,5 +1,6 @@
 package com.appsdeveloperblog.ws.emailnotification.handler;
 
+import com.appsdeveloperblog.ws.emailnotification.error.NotRetryableException;
 import com.appsdeveloperblog.ws.products.ProductCreatedEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -18,27 +19,33 @@ public class ProductCreatedEventHandler {
 
     @KafkaListener(topics = "product-created-events-topic")
     public void handle(ConsumerRecord<String, ProductCreatedEvent> consumerRecord) {
+
+        if (true) {
+            throw new NotRetryableException("Simulated error");
+        }
         ProductCreatedEvent productCreatedEvent = consumerRecord.value();
 
         // Decode the decimal logical type from the Avro "price" field
-        BigDecimal price = decodeDecimal(productCreatedEvent.getPrice(), 2);
+        // BigDecimal price = decodeDecimal(productCreatedEvent.getPrice(), 2);
 
         log.info("****** Received ProductCreatedEvent ******");
         log.info("Product ID: {}", productCreatedEvent.getProductId());
         log.info("Title: {}", productCreatedEvent.getTitle());
-        log.info("Price: {}", price); // Log deserialized BigDecimal
+        log.info("Title: {}", productCreatedEvent.getPrice());
+        //log.info("Price: {}", price); // Log deserialized BigDecimal
+
         log.info("Quantity: {}", productCreatedEvent.getQuantity());
 
         log.info("Received a new event: {}", productCreatedEvent.getTitle());
     }
 
     // Decode ByteBuffer to BigDecimal
-    private BigDecimal decodeDecimal(ByteBuffer byteBuffer, int scale) {
-        // Convert ByteBuffer to BigInteger
-        BigInteger unscaledValue = new BigInteger(byteBuffer.array());
-
-        // Apply scale to BigInteger to get BigDecimal
-        return new BigDecimal(unscaledValue, scale);
-    }
+//    private BigDecimal decodeDecimal(ByteBuffer byteBuffer, int scale) {
+//        // Convert ByteBuffer to BigInteger
+//        BigInteger unscaledValue = new BigInteger(byteBuffer.array());
+//
+//        // Apply scale to BigInteger to get BigDecimal
+//        return new BigDecimal(unscaledValue, scale);
+//    }
 
 }
